f9865ad9dcbc2f1e93698b73971b9f11efa044dc
koderoom <koderoom.dev@gmail.com>
2021-01-22 14:25:38 +0530

    ui for signup.

--- src/App.js
+++ src/App.js
@@ -5,7 +5,30 @@ function App() {
 }
 
 function Signup() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <h3>Sign Up Form</h3>
+
+      <div>
+        Name <input type="text" />
+      </div>
+
+      <div>
+        Pswd <input type="text" />
+      </div>
+
+      <div>
+        Email <input type="text" />
+      </div>
+      <div>
+        Mobile <input type="text" />
+      </div>
+
+      <div>
+        <button>Register</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
